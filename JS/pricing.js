8b412e6b8f7c8449695499344807e213d0b9a6f2
ligarunge <liga.runge@gmail.com>
2022-02-07 11:59:22 +0200

    hj

---
+++ JS/pricing.js
@@ -0,0 +1,41 @@
+const addToCart = () => {
+    document.getElementById('shoppingCart').innerHTML = 
+    `<div class="row centered">
+    <div class="col-4">
+     <img src="imagesMugs/CSS2.jpg" alt="" class="img-fluid rounded">
+    </div>
+    <div class="col-4 text-dark">
+        <select>
+            <option selected>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+        </select>
+    </div>
+    <div class="col-4 text-dark">
+   <h6>€14.99</h6>  
+    </div>`;
+    document.getElementById('total').innerHTML = '€14.99';
+}
+
+const addWhyWorksToCart = () => {
+    document.getElementById('shoppingCart').innerHTML += 
+    `<div class="row centered">
+    <div class="col-4">
+     <img src="imagesMugs/WhyWorks2.jpg" alt="" class="img-fluid rounded">
+    </div>
+    <div class="col-4 text-dark">
+        <select>
+            <option selected>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+        </select>
+    </div>
+    <div class="col-4 text-dark">
+   <h6>€14.99</h6>  
+    </div>`;
+    document.getElementById('total').innerHTML = '€29.98';
+}
